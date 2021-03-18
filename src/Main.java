93ad873df064c7fec6794f23b975afcc6568e839
jukkty <dima.iachimov@gmail.com>
2021-03-18 11:58:20 +0200

    another commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        CreditPaymentService service1 = new CreditPaymentService();
-        CreditPaymentService2 service2 = new CreditPaymentService2();
-        CreditPaymentService3 service3 = new CreditPaymentService3();
-        int year = 1;
-        double credit = 1_000_000;
-        double rate = 9.99;
-        double monthPayment1 = service1.calculate(year,credit,rate);
-        double monthPayment2 = service2.calculate(year,credit,rate);
-        double monthPayment3 = service3.calculate(year,credit,rate);
-        System.out.println(monthPayment1);
-        System.out.println(monthPayment2);
-        System.out.println(monthPayment3);
+        CreditPaymentService service = new CreditPaymentService();
+        int year;
+        double credit;
+        double rate;
+        System.out.println(service.calculate(year = 1,credit = 1_000_000,rate = 9.99));
+        System.out.println(service.calculate(year = 2,credit = 1_000_000,rate = 9.99));
+        System.out.println(service.calculate(year = 3,credit = 1_000_000,rate = 9.99));
     }
 }
